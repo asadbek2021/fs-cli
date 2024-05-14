10e9f9790f05dd4aab09aa4df3003856043e87db
Asadbek Raimov <asadbek.r@payme.uz>
2024-05-15 02:19:18 +0500

    feat: add user validation for name and add line breaks in messages

--- src/user.js
+++ src/user.js
@@ -4,15 +4,18 @@ export class User {
     #name
 
     constructor(name) {
+        if(!name) {
+            throw new Error('Invalid input. Username was not provided')
+        }
+
         this.#name = name;
     }
 
     greet() {
-        return `Welcome to the File Manager, ${this.#name}!\n`;
+        return `Welcome to the File Manager, ${this.#name}!\n---------------------------- \n`;
     }
 
     bye() {
-        return `Thank you for using File Manager, ${this.#name}, goodbye!\n`;
+        return `\nThank you for using File Manager, ${this.#name}, goodbye!\n`;
     }
-
 }
