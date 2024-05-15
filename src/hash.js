349b25186a30525a065c6d684040cda64afa2eb7
Asadbek Raimov <asadbek.r@payme.uz>
2024-05-16 00:12:23 +0500

    feat: add hash methods

--- src/hash.js
+++ src/hash.js
@@ -1,11 +1,32 @@
+import Crypto from "crypto";
+
 export class Hash {
+    #hash;
+    #crypto = Crypto;
 
 
-    createMD5Hash() {
+    createHash(algorithm) {
+        if (this.#hash){
+            throw new Error("Hash already exists")
+        }
+        this.#hash = this.#crypto.createHash(algorithm);
 
+        return this;
     }
 
-    convertToHex() {
-        
+    appendHash(chunk) {
+        if (!this.#hash){
+            throw new Error("Hash doesn't exist")
+        }
+
+        this.#hash.update(chunk)
+    }
+
+    digestHash(type) {
+        if (!this.#hash){
+            throw new Error("Hash doesn't exist")
+        }
+
+        return this.#hash.digest(type);
     }
 }
