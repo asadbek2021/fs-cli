9740d408d5b4c7dc0483106334c4fb33671aba32
Asadbek Raimov <asadbek.r@payme.uz>
2024-05-16 00:14:12 +0500

    refactor: cleaning code

--- src/index.js
+++ src/index.js
@@ -1,24 +1,40 @@
-import { Messenger } from "./messenger.js";
-import { FileSystem } from "./fs.js";
-import { OperatingSystem } from "./os.js";
-import { Process } from "./process.js";
-import { Commander } from "./commander.js";
 import { User } from "./user.js";
+import { Hash } from "./hash.js";
+import { FileSystem } from "./fs.js";
+import { Process } from "./process.js";
+import { OperatingSystem } from "./os.js";
+import { Messenger } from "./messenger.js";
+import { Commander } from "./commander.js";
+import { BrotliCompress } from "./brotli-archive.js";
+import { CompressTransformStream } from "./streams/compress-transform.js";
+import { DecompressTransformStream } from "./streams/decompress-transform.js";
 
 const newProcess = new Process();
+const newOS = new OperatingSystem()
 const messenger = new Messenger(
     newProcess, 
-    new User(newProcess.argumentValue("username"))
+    new User(
+        newProcess.argumentValue("username")
+    )
 );
 
 const commander = new Commander(
-    messenger,
     new FileSystem(
-        newProcess
+        newProcess,
+        new Hash(),
+        new CompressTransformStream(
+            new BrotliCompress()
+        ),
+        new DecompressTransformStream(
+            new BrotliCompress()
+        )
     ),
-    new OperatingSystem(),
+    newOS,
     newProcess
 );
+
+
+newProcess.changeWorkingDirectory(newOS.homeDirectory());
 
 newProcess
     .onExit(() => messenger.finish())
@@ -30,3 +46,4 @@ newProcess
 
 
 messenger.greetWithUsername()
+messenger.afterEachMessage();
